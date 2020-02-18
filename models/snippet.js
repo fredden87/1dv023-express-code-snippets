8a481fa717bd9ab7677f68e3a1158f00d1950294
Fredrik Norrman <fn222ep@student.lnu.se>
2020-02-18 10:04:46 +0100

    snippetschema implemened

---
+++ models/snippet.js
@@ -0,0 +1,30 @@
+/**
+ * Mongoose model Snippet.
+ *
+ * @author Fredrik Norrman
+ * @version 1.0.0
+ */
+
+'use strict'
+
+const mongoose = require('mongoose')
+
+// Schema representing a code snippet.
+const snippetSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  snippet: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+// Create a model using the schema.
+const Snippet = mongoose.model('Snippet', snippetSchema)
+
+// Exports.
+module.exports = Snippet
