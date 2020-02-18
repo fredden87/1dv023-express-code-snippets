f669ec6c8db1d59f41238f91a1ce3c49825d49d8
Fredrik Norrman <fn222ep@student.lnu.se>
2020-02-18 04:16:46 +0100

    Login route implemented

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -24,6 +24,23 @@ loginController.index = (req, res) => {
 
 loginController.auth = async (req, res) => {
   if (req.body.username.length > 0 && req.body.password.length > 2) {
+    try {
+      const username = req.body.username
+      const user = await User.findOne({ username })
+      if (user && user.password === req.body.password) {
+        req.session.flash = { type: 'success', text: 'Successfully logged in!' }
+        res.redirect('..')
+      } else {
+        req.session.flash = { type: 'danger', text: 'Invalid username or password!' }
+        res.redirect('.')
+      }
+    } catch (error) {
+      req.session.flash = { type: 'danger', text: 'Invalid username or password!' }
+      res.redirect('.')
+    }
+  } else {
+    req.session.flash = { type: 'danger', text: 'Invalid username or password!' }
+    res.redirect('.')
   }
 }
 
