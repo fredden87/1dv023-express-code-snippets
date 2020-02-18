51e772fbd0276a3f3939aafb4b0e65ef9eac604d
Fredrik Norrman <fn222ep@student.lnu.se>
2020-02-18 07:39:24 +0100

    Hashing and salting passwords implemented

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 // Schema representing a user account.
 const userSchema = new mongoose.Schema({
@@ -37,11 +38,16 @@ const userSchema = new mongoose.Schema({
 
 userSchema.statics.authenticate = async function (username, password) {
   const user = await this.findOne({ username })
-  if (!user || !(password === user.password)) {
+  if (!user || !(await bcrypt.compare(password, user.password))) {
     throw new Error('Invalid login attempt.')
   }
   return user
 }
+
+// Generate salt and hash before save.
+userSchema.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 8)
+})
 
 // Create a model using the schema.
 const User = mongoose.model('User', userSchema)
